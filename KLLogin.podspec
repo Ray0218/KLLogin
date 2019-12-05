6201cc2e6ae936a31b581801ae2a8cfe1d28b537
吴孔亮 <ray_ios@163.com>
2019-12-05 16:16:32 +0800

    2

--- KLLogin.podspec
+++ KLLogin.podspec
@@ -2,7 +2,7 @@
 Pod::Spec.new do |s|
 
 s.name         = "KLLogin"
-s.version ="1"
+s.version ="2"
 s.summary      = "登录"
 
 s.description  = <<-DESC
--- upload.sh
+++ upload.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-VersionString="grep -E 's.version.*=' KLLogin.podspec"
+VersionString=`grep -E 's.version.*=' KLLogin.podspec`
 VersionNumber=`tr -cd 0-9 <<<"$VersionString"`
- 
+
 NewVersionNumber=$(($VersionNumber + 1))
 LineNumber=`grep -nE 's.version.*=' KLLogin.podspec | cut -d : -f1`
 sed -i "" "${LineNumber}s/${VersionNumber}/${NewVersionNumber}/g" KLLogin.podspec
 
-echo " ss  ${VersionString}current version is ${VersionNumber}, new version is ${NewVersionNumber}"
+echo "current version is ${VersionNumber}, new version is ${NewVersionNumber}"
 
 git add .
 git commit -am ${NewVersionNumber}
 git tag ${NewVersionNumber}
 git push origin master --tags
-pod repo push CustomPrivatePods KLLogin.podspec --allow-warnings --use-libraries --use-modular-headers
+pod repo push CustomPrivatePods KLLogin.podspec --verbose --allow-warnings --use-libraries --use-modular-headers
+
