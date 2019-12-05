248e93b95be8cdb5bd908616dc98dc6bd1dbd391
吴孔亮 <ray_ios@163.com>
2019-12-05 15:56:52 +0800

    1

--- upload.sh
+++ upload.sh
@@ -2,11 +2,12 @@
 
 VersionString="grep -E 'spec.version.*=' KLLogin.podspec"
 VersionNumber=`tr -cd 0-9 <<<"$VersionString"`
+
 NewVersionNumber=$(($VersionNumber + 1))
 LineNumber=`grep -nE 'spec.version.*=' KLLogin.podspec | cut -d : -f1`
 sed -i "" "${LineNumber}s/${VersionNumber}/${NewVersionNumber}/g" KLLogin.podspec
 
- echo "current version is ${VersionNumber}, new version is ${NewVersionNumber}"
+echo "current version is ${VersionNumber}, new version is ${NewVersionNumber}"
 
 git add .
 git commit -am ${NewVersionNumber}
