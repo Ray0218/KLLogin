6fd3de199df9d6092ddaebc8f2f459cb6602091c
吴孔亮 <ray_ios@163.com>
2019-12-06 14:58:09 +0800

    3.0.5

---
+++ KLLogin/Class/UserModel.h
@@ -0,0 +1,17 @@
+//
+//  UserModel.h
+//  KLLogin
+//
+//  Created by WKL on 2019/12/6.
+//  Copyright © 2019 Ray. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UserModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ KLLogin/Class/UserModel.m
@@ -0,0 +1,13 @@
+//
+//  UserModel.m
+//  KLLogin
+//
+//  Created by WKL on 2019/12/6.
+//  Copyright © 2019 Ray. All rights reserved.
+//
+
+#import "UserModel.h"
+
+@implementation UserModel
+
+@end
